f059b06c2fea9e3ded936514d5f04eebaf40a1c6
George Zhang <george.itg@gmail.com>
2017-10-15 22:27:17 -0600

    initialization
    initial commit

---
+++ Excercises/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Excercises.Startup))]
+namespace Excercises
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
